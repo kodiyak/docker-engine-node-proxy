1552ba0cf5b80e582d111fbfed33a98738df5ccb
kodiyak <mathewsto51@gmail.com>
2022-04-16 23:21:27 -0300

    init

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import cors from "cors";
+import DockerModem from "docker-modem";
+import express, { json, Router } from "express";
+
+const app = express();
+const router = Router();
+
+router.all(`/docker/*`, (req, res) => {
+  const request = {
+    method: req.method,
+    path: req.url.replace("/docker", "*"),
+    dial: req.body.dial,
+  };
+
+  const modem = new DockerModem({
+    socketPath: "/var/run/docker.sock",
+  });
+  modem.dial(
+    {
+      ...request.dial,
+    },
+    (err, dockerResponse) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+      res.status(200).send(dockerResponse);
+    }
+  );
+
+  res.json(request);
+});
+
+app.use(json());
+app.use(cors());
+app.use(router);
+
+app.listen(3039, () => {
+  console.log(`server litening at port 3039`);
+});
